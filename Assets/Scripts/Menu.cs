b876d7ea17148e19ec2b38724ad974dae1541db7
kevinh-a <kevin.cong.ha@gmail.com>
2023-11-27 00:46:21 -0800

    MainMenu Scene and UI elements

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Credit to the background image
+    // https://www.vecteezy.com/free-vector/ruined-city
+    // This Menu Class will mostly consist of buttons for the main menu
+    public void PlayButton()
     {
-        
+        //Loads the nextindexed scene
+        //SceneManager.LoadScene();
     }
 
     // Update is called once per frame
-    void Update()
+    public void Quit()
     {
-        
+        //Quits the game application
+        Application.Quit();
     }
 }
