24cbbb2111d583ccbcd84a3251840eea84006ca1
ahhhThomas <146419438+ahhhThomas@users.noreply.github.com>
2023-11-26 18:02:11 -0800

    Attempted fixing player jump
    attempted fixing the wonky player jump and bugs in the playercontroller class

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -14,13 +15,13 @@ public class PlayerController : MonoBehaviour
     private float boost;
 
     private Vector3 movement;
-    private bool isJumping;
+    private bool grounded;
 
     // Start is called before the first frame update
     private void Start()
     {
         body = GetComponent<Rigidbody2D>();
-        isJumping = false;
+        grounded = true;
     }
 
     // Update is called once per frame
@@ -40,7 +41,7 @@ public class PlayerController : MonoBehaviour
             //body.velocity = Vector2.left * speed;
         }
 
-        if((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space)) && isJumping == false)
+        if((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space)) && grounded == true)
         {
             movement += new Vector3(0, boost, 0);
             //transform.position += new Vector3(0, boost * Time.deltaTime, 0);
@@ -55,23 +56,31 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        body.AddForce(movement);
+        body.AddForce(movement, ForceMode2D.Force);
         movement = Vector3.zero;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Platform")
         {
-            isJumping = false;
+            grounded = true;
         }
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
+    private void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Platform")
         {
-            isJumping = true;
+            grounded = false;   
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            body.AddForce(new Vector2(0, boost));
         }
     }
 }
