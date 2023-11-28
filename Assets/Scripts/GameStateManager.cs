c16797af1695691f71eba58f02a4ec596e0563bc
kevinh-a <kevin.cong.ha@gmail.com>
2023-11-27 18:02:56 -0800

    GameStateManager Added

---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class GameStateManager : MonoBehaviour
+{
+    public static Action OnGameOver;
+
+    private static GameStateManager _instance;
+
+
+    void Start()
+    {
+        //Setup for Singleton
+        if(_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(_instance);
+        }
+        else
+        {
+            if(_instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    public static void GameOver()
+    {
+        Time.timeScale = 0f;
+        OnGameOver();
+    }
+}
