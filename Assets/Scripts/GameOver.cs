4b446a2266c26171a2b1fd86b1762b4f55e42dbe
kevinh-a <kevin.cong.ha@gmail.com>
2023-11-27 18:44:11 -0800

    GameOverScreen Finished

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //subscribes the action in GameStateManager to GameOver
+        GameStateManager.OnGameOver += Open;
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    public void OnDestroy()
+    {
+        GameStateManager.OnGameOver -= Open;
+    }
+
+    private void Open()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        gameObject.SetActive(false);
+        GameStateManager.GameOver();
+    }
+
+    public static void BackToMenu()
+    {
+        SceneManager.LoadScene("MainestMenu");
+    }
+}
