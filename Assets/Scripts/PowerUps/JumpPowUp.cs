7c903b2be845e31c3b211c8e929e7d08399cbec4
kevinh-a <kevin.cong.ha@gmail.com>
2023-12-02 23:50:40 -0800

    Health and Jump Power up
    - Healing power up finished
    -Jump power up finished

---
+++ Assets/Scripts/PowerUps/JumpPowUp.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPowUp : MonoBehaviour
+{
+    //How much of a jump boost is obtained
+    [SerializeField]
+    private float jumpBoost;
+
+    //How long the boost will last for
+    [SerializeField]
+    private float duration;
+
+}
