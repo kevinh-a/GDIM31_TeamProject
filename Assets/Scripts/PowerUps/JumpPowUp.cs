3e49ad7de977d96e9b06ca9aaeeb59dca8a7c437
kevinh-a <kevin.cong.ha@gmail.com>
2023-12-03 22:24:35 -0800

    Jump and Health power ups done

--- Assets/Scripts/PowerUps/JumpPowUp.cs
+++ Assets/Scripts/PowerUps/JumpPowUp.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JumpPowUp : MonoBehaviour
+public class JumpPowUp : PowerUpBase
 {
     //How much of a jump boost is obtained
     [SerializeField]
@@ -12,4 +12,24 @@ public class JumpPowUp : MonoBehaviour
     [SerializeField]
     private float duration;
 
+    private float startTime;
+    private PlayerController playerCon;
+
+    public override void Apply(PlayerController player)
+    {
+        //Initializes exact time the powUp is picked up, and gives it to the player char
+        startTime = Time.time;
+        playerCon = player;
+        playerCon.AugmentJump(jumpBoost);
+    }
+
+    private void Update()
+    {
+        //If the duration has been exceeded in time, end the effects
+        if(Time.time - startTime > duration)
+        {
+            playerCon.AugmentJump(-jumpBoost);
+            Destroy(gameObject);
+        }
+    }
 }
