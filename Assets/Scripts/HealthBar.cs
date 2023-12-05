af5727bc5f05a2c84b84269d39ee7ad57bd4969a
kevinh-a <kevin.cong.ha@gmail.com>
2023-12-04 18:49:14 -0800

    health bar wip2

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -10,10 +10,22 @@ public class HealthBar : MonoBehaviour
     [SerializeField]
     private Text livesCounter;
 
+    [SerializeField]
+    private Image[] heartsSprites;
+
     // Start is called before the first frame update
     void Start()
     {
         lives = GameStateManager.GetLives();
+        CreateHearts(3);
+    }
+
+    private void CreateHearts(int numOfHearts)
+    {
+        for (int i = 0; i < numOfHearts; i++)
+        {
+            //Instantiate(heartsSprites, transform);
+        }
     }
 
     // Update is called once per frame
