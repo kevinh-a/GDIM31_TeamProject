8d245e97b52d5446fc3b8e692c1bf991d1d838a2
kevinh-a <kevin.cong.ha@gmail.com>
2023-12-05 14:29:43 -0800

    pause menu implemented

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -8,32 +8,57 @@ public class PauseMenu : MonoBehaviour
     [SerializeField]
     private GameObject pauseMenuObj;
 
+    //Tells the game if the game is already paused
+    private static bool gameIsPaused;
+
     // Start is called before the first frame update
     void Start()
     {
         pauseMenuObj.SetActive(false);
+        gameIsPaused = false;
     }
 
-    void Resume()
+    void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //If the game is paused and esc is pressed, resume the game
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        //Resumes time, and hides the pause screen
         Time.timeScale = 1f;
         pauseMenuObj.SetActive(false);
+        gameIsPaused = false;
     }
 
-    void Pause()
+    public void Pause()
     {
+        //stops time, and shows the pause screen
         Time.timeScale = 0f;
         pauseMenuObj.SetActive(true);
+        gameIsPaused = true;
     }
 
-    void BackToMenu()
+    public void BackToMenu()
     {
+        //Resumes time, and heads back to the main menu
         Time.timeScale = 1f;
         SceneManager.LoadScene("MainestMenu");
     }
 
-    void Quit()
+    public void PauseQuit()
     {
+        //Quits the application
         Application.Quit();
     }
 }
