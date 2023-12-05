44891fe73bf7accc90705a771c06add75b6e3330
kevinh-a <kevin.cong.ha@gmail.com>
2023-12-05 13:33:52 -0800

    pausemenu script

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pauseMenuObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenuObj.SetActive(false);
+    }
+
+    void Resume()
+    {
+        Time.timeScale = 1f;
+        pauseMenuObj.SetActive(false);
+    }
+
+    void Pause()
+    {
+        Time.timeScale = 0f;
+        pauseMenuObj.SetActive(true);
+    }
+
+    void BackToMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainestMenu");
+    }
+
+    void Quit()
+    {
+        Application.Quit();
+    }
+}
